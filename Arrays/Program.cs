324ecd967468b28c12e145a618305d99598a49e0
Tart Heiti <Heiti.Tart@itella.com>
2023-02-18 19:38:34 +0200

    Updated

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//string[] fruits = { "apples", "bananas", "oranges" };
+
+
+string[] fruit = new string[3];
+
+Console.WriteLine("Enter first fruit:");
+fruit[0] = Console.ReadLine();
+
+Console.WriteLine("Enter second fruit:");
+fruit[1] = Console.ReadLine();
+
+Console.WriteLine("Enter third fruit:");
+fruit[2] = Console.ReadLine();
+
+Console.WriteLine($" {fruit[0]},{fruit[1]},{fruit[2]}" );
+
+int fruitArrayLength = fruit.Length;
+Console.WriteLine($"There are: {fruitArrayLength} in your refrigerator" );
