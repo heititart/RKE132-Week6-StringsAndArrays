6ebc4c8f712a452438f1d7bd0eb7638f3855887d
Tart Heiti <Heiti.Tart@itella.com>
2023-02-18 16:34:07 +0200

    Add week 6 code

---
+++ Week6StringAndArrays_Ver.2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Enter word or sentence:");
+
+
+string hello = Console.ReadLine();
+
+int stringLength = hello.Length;
+
+Console.WriteLine("");
+
+hello = hello.Trim(); //string trimmedstring = hello.Trim();
+
+char firstLetter = hello[0];
+
+Console.WriteLine($"Entered value is: {hello}");
+Console.WriteLine($"first letter is: {firstLetter}");
+
+Console.WriteLine();
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+Console.WriteLine($"Correted sentence Is:{hello}");
+Console.WriteLine($"first letter is: {hello[0]}");
+
+
+int wordCounter = 1;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    {
+        wordCounter++;
+    }
+}
+
+
+Console.WriteLine($"Word count in: {hello}: {wordCounter} words");
+
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Sisesta sõna või lause:");
+
+string hello = Console.ReadLine();
+
+int stringLength = hello.Length;
+
+//string trimmedstring = hello.Trim();
+
+hello = hello.Trim();
+
+int wordCounter = 1;
+
+for (int i = 0; i < hello.Length; i++)
+{
+	if (hello[i] == ' ')
+	{
+		wordCounter++;
+	}
+}
+
+
+Console.WriteLine($"Word count in: {hello}: {wordCounter} words");
+
